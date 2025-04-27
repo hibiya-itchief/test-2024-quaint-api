abe0cb34a5163e1a4e97055a37b8485b80c6580d
MRharu0310 <sharuharu0310@gmail.com>
2025-04-27 16:49:05 +0900

    origins編集

--- app/main.py
+++ app/main.py
@@ -67,7 +67,7 @@ app = FastAPI(
     version="0.1.0",
 )
 ### TODO 同一オリジンにアップロードしてcorsは許可しない
-origins = ["http://localhost:3000"]
+origins = ["http://localhost:3000","http://test-2024-quaint-app.pages.dev"]
 
 app.add_middleware(
     CORSMiddleware,
